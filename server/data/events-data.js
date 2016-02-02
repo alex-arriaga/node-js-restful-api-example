9280e1eceaf529645a51d71accb82eb9d8fc6aa5
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-02-02 16:50:07 -0500

    Added +events

--- server/data/events-data.js
+++ server/data/events-data.js
@@ -4,32 +4,32 @@ var events = [{
     "url": "index.html",
     "overrideURL": "",
     "linkType": "",
-    "title": "Sports 1 enim ad minim veniam",
-    "summary": "Lorem ipsum dolor sit amet"
+    "title": "College Basketball: Georgetown at Butler",
+    "summary": "Georgetown at Butler. Georgetown's D'Vauntes Smith-Rivera squares off with Butler's Kellen Dunham in a battle of senior guards. The Hoyas took two of three meetings with the Bulldogs last season."
 }, {
     "topics": "",
     "thumbnail": "/img/b-2.jpeg",
     "url": "index.html",
     "overrideURL": "",
     "linkType": "",
-    "title": "Business 1 ipsum dolor sit",
-    "summary": "Lorem ipsum dolor sit amet"
+    "title": "IBM Connect 2016",
+    "summary": "Hear from executives and clients who will share their insights and experiences. Learn how to capitalize on the disruptive, fast-moving transformations like cloud, mobile, cognitive and analytics that are changing how we connect and collaborate."
 }, {
     "topics": "business",
     "thumbnail": "/img/b-1.jpeg",
     "url": "index.html",
     "overrideURL": "http://base22.com",
     "linkType": "external",
-    "title": "Business 2 ut enim ad minim veniam",
-    "summary": "Ut enim ad minim veniam"
+    "title": "WIRED Business Conference 2016",
+    "summary": "On June 16, the WIRED Business Conference 2016 will take you beyond the usual tech-conference suspects. Our event is designed to introduce you to the true doers in the modern business universe. "
 }, {
     "topics": "sports",
     "thumbnail": "/img/s-2.jpeg",
     "url": "index.html",
     "overrideURL": "",
     "linkType": "",
-    "title": "Sports 2",
-    "summary": "Ut enim ad minim veniam",
+    "title": "BASE Jumping",
+    "summary": "is very extreme activity that includes a parachute (can be used both parachute and wingsuit) to jump from fixed objects, with unopened parachute like skydiving. The acronym “B.A.S.E.” stands for: Building, Antenna, Span, Earth – four categories of objects from which B.A.S.E. jumper can jump."
 }, {
     "topics": "",
     "thumbnail": "/img/tr-3.jpeg",
@@ -44,8 +44,8 @@ var events = [{
     "url": "index.html",
     "overrideURL": "",
     "linkType": "",
-    "title": "Sports 3",
-    "summary": "Lorem ipsum dolor sit amet"
+    "title": "2016 Olympic Trials",
+    "summary": "Qualifying time standards for the 2016 Olympic Trials were announced Thursday, September 18, at 2 p.m. EDT, via a live webcast from the United States Aquatic Sports Convention in Jacksonville, Florida. It can be viewed via usaswimming.org/trials."
 }, {
     "topics": "business",
     "thumbnail": "/img/b-4.jpeg",
