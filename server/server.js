3d0d63064e3dac3b3f9bbdfeccb3a254d77c87f5
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-09-29 10:03:05 -0500

    Add CORS to the express.js app

--- server/server.js
+++ server/server.js
@@ -5,14 +5,25 @@ var _ = require('lodash');
 var eventsRouter = require('./routers/events-router');
 
 var app = express();
+var port = 3000;
 
 app.use(morgan('dev'));
 app.use(express.static('client'));
+
+
+// Enable CORS on ExpressJS
+// In your ExpressJS app on node.js, do the following with your routes:
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,recording-session");
+    next();
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/events', eventsRouter);
 
-app.listen(4000);
-console.log("Running app on port 4000. Visit: http://localhost:4000/");
+app.listen(port);
+console.log("Running app on port port. Visit: http://localhost:/" + port);
 
