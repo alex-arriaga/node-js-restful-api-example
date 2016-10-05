26dc89463b8867125454f3174bc7dbcab5a5ed8d
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-10-05 11:15:44 -0500

    Improve documentation about this RESTful API example

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var _ = require('lodash');
 var eventsRouter = require('./routers/events-router');
 
 var app = express();
@@ -11,8 +10,8 @@ app.use(morgan('dev'));
 app.use(express.static('client'));
 
 
-// Enable CORS on ExpressJS
-// In your ExpressJS app on node.js, do the following with your routes:
+// Enable CORS on ExpressJS to avoid cross-origin errors when calling this server using AJAX
+// We are authorizing all domains to be able to manage information via AJAX (this is just for development)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,recording-session");
@@ -25,5 +24,5 @@ app.use(bodyParser.json());
 app.use('/events', eventsRouter);
 
 app.listen(port);
-console.log("Running app on port port. Visit: http://localhost:/" + port);
+console.log("Running app on port port. Visit: http://localhost:" + port + "/");
 
